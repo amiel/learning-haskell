b1de87d21b85fafec6d870cb091f840583975dbb
Amiel Martin <amiel.martin@gmail.com>
2009-12-19 10:52:59 -0900

    split shouldnt have an axtra empty string

--- split.hs
+++ split.hs
@@ -12,7 +12,7 @@ split2 (c, "") = ""
 split2 (c, (x:xs)) = if x == c then xs else split2(c, xs)
 
 split :: (Char, String) -> [String]
-split (c, "") = [""]
+split (c, "") = []
 split (c, xs) = [split1(c, xs)] ++ split(c, split2(c, xs))
 
 main = print(split(',', "foo, bar, baz"))
