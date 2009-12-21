79d85fe84801a7824423d766d8a49dfa9215d3dc
Amiel Martin <amiel.martin@gmail.com>
2009-12-21 11:43:51 -0900

    ceaser crypt now works

--- ceaser.hs
+++ ceaser.hs
@@ -1,8 +1,8 @@
--- ceaser but without modulo
+
 ceaser :: Int -> String -> String
 ceaser i [] = []
-ceaser i (x:xs) = [toEnum(fromEnum x + i)] ++ ceaser i xs
+ceaser i (x:xs) = toEnum(((fromEnum x + i - fromEnum('a')) `mod` 26) + fromEnum('a')) : ceaser i xs
 
 
-main = print(ceaser 1 "abc")
+main = print(ceaser 4 "abcyz")
 
